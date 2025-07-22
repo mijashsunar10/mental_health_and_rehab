64f7b505eab35f1fe17dbfab63b42a6b67ef7930
Sandesh Pahari <sandeshpahari05@gmail.com>
2025-07-22 18:22:21 +0545

    faq model

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Faq extends Model
 {
-    //
+   protected $fillable = ['question', 'answer', 'slug'];
+
+    
+    // Automatically generate a slug from the question
+    protected static function boot()
+    {
+        parent::boot();
+        static::saving(function ($faq) {
+            $faq->slug = Str::slug($faq->question);
+        });
+    }
 }
