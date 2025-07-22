a28f6ba215c4c5b0295b3ffbb587c02147cb0521
Sandesh Pahari <sandeshpahari05@gmail.com>
2025-07-22 18:49:57 +0545

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\FaqController;
 
 Route::get('/', function () {
     return view('frontend.home.index');
@@ -17,6 +18,20 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+Route::controller(FaqController::class)->group(function () {
+    // Publicly accessible route
+    Route::get('faqs', 'index')->name('faqs.index');
+
+    // Authenticated and verified routes
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('faqs/create', 'create')->name('faqs.create');
+        Route::post('faqs', 'store')->name('faqs.store');
+        Route::get('faqs/{faq:slug}/edit', 'edit')->name('faqs.edit');
+        Route::put('faqs/{faq:slug}', 'update')->name('faqs.update');
+        Route::delete('faqs/{faq:slug}', 'destroy')->name('faqs.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
